402cbe40024cf7bf9a3fde72dcb5da1929f53ff3
Mac Massie <masswm24@wfu.edu>
2024-12-03 13:47:39 -0500

    added tabs

--- import streamlit as st.py
+++ import streamlit as st.py
@@ -4,4 +4,22 @@ import numpy as np
 import sqlite3
 
 st.header("Group 8 Final Project")
-conn = sqlite3.connect("Cleaned_Mental_Health_data.db")
+conn = sqlite3.connect("Mental_Health_data.db")
+
+st.subheader('Tabbed Questions by person')
+tab1, tab2, tab3, tab4, tab5 = st.tabs(["Julia", "Mac", "David", "Koise", "Chris"])
+
+with tab1:
+    st.header("Julia's Questions")
+
+with tab2:
+    st.header("Mac's Questions")
+
+with tab3:
+    st.header("David's Questions")
+
+with tab1:
+    st.header("Koise's Questions")
+
+with tab1:
+    st.header("Chris's Questions")
